70efb4aabed0d2e87cedc7845fcb223a01faf09a
AJ <arunjagadishsd@gmail.com>
2019-10-18 14:26:56 +0530

    Added the lwc

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    @track sessionId;
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+        this.state = 'details';
+        window.history.pushState('details', null);
+    }
+    get isStateList() {
+        return this.state === 'list';
+    }
+    get isStateDetails() {
+        return this.state === 'details';
+    }
+    @track state;
+    constructor() {
+        super();
+        this.state = 'list';
+        window.history.replaceState('list', null, '');
+        window.onpopstate = event => {
+            if (event.state) {
+                this.state = event.state;
+            }
+        };
+    }
+}
--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -7,4 +7,19 @@ export default class SessionList extends LightningElement {
             this.sessions = this.allSessions = result;
         });
     }
+    handleSearchKeyInput(event) {
+        const searchKey = event.target.value.toLowerCase();
+        this.sessions = this.allSessions.filter(
+            session => session.name.toLowerCase().includes(searchKey)
+        );
+    }
+    handleSessionClick(event) {
+        console.log('arun');
+        
+        const index = event.currentTarget.dataset.index;
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: this.sessions[index].id
+        });
+        this.dispatchEvent(navigateEvent);
+    }
 }
